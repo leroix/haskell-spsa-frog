f05762d767397e964ae88a6ec8820372b32f024e
Leroix <leroix08@gmail.com>
2014-01-15 09:05:32 -0700

    write loss function, trace tools and retrieve function

--- src/Math/FROG/Retrieval.hs
+++ src/Math/FROG/Retrieval.hs
@@ -1,9 +1,35 @@
 module Math.FROG.Retrieval
 ( retrieve
+, calcLoss
 ) where
 
-import Numeric.LinearAlgebra (Vector, Matrix, fromList)
+import qualified Numeric.LinearAlgebra as LA
+import qualified Math.Optimization.SPSA as SPSA
 
-retrieve :: Matrix Double -> IO (Vector Double)
-retrieve measurement = do
-    return $ fromList [1::Double,2,3,4]
+import qualified Math.FROG.Tools as FR
+import Math.FROG.Types (Nonlinearity)
+
+retrieve :: Nonlinearity -> LA.Matrix Double -> IO (LA.Vector Double)
+retrieve nonlin measurement = do
+    let sz = LA.rows measurement
+    let (gainA, gainC) = SPSA.semiautomaticTuning 0.0001 0.05
+    let lossFn = calcLoss nonlin measurement
+
+    spsa <- SPSA.mkUnconstrainedSPSA lossFn gainA gainC sz
+    initialGuess <- FR.mkInitialGuess sz
+
+    return $ SPSA.optimize spsa 1000 initialGuess
+    
+
+
+
+calcLoss :: Nonlinearity -> LA.Matrix Double -> LA.Vector Double -> Double
+calcLoss nonlin meas v = (/ (fromIntegral $ LA.rows meas)) . sqrt 
+                                                           . LA.sumElements $ sqDiff
+    where
+    sz = (LA.dim v) `quot` 2
+    mags = LA.subVector 0 sz v
+    angs = LA.subVector sz sz v
+    field = LA.buildVector sz (\k -> (LA.mkPolar (mags LA.@> k) (angs LA.@> k)))
+    (signal, gate) = FR.mkSigGate nonlin field field
+    sqDiff = (^2) . abs $ (meas - FR.mkTrace signal gate)
--- src/Math/FROG/Tools.hs
+++ src/Math/FROG/Tools.hs
@@ -1,73 +1,59 @@
 module Math.FROG.Tools
 ( mkTrace
-
-, mkPGgate
-, mkSHGgate
-, mkSDgate
-, mkTHGgate
-
-, mkPGfield
-, mkSHGfield
-, mkSDfield
-, mkTHGfield
-
+, mkSigGate
 , shift
+, mkInitialGuess
 ) where
 
+import qualified System.Random as R
 import qualified Numeric.LinearAlgebra as LA
 import qualified Numeric.GSL.Fourier as F
 import qualified Foreign
 
+import Math.FROG.Types (Nonlinearity(..))
+
+
 type ComplexSignal = LA.Vector (LA.Complex Double)
 type Trace = LA.Matrix Double
-type SignalIntensity = LA.Vector Double
+
 
 mkTrace :: ComplexSignal -> ComplexSignal -> Trace
 mkTrace field gate = LA.fromColumns $ map atEachDelayApply [upper,upper-1..lower]
     where
     atEachDelayApply = (^2) . (LA.mapVector LA.magnitude) 
-                             . (flip shift upper) 
-                             .  F.fft 
-                             . (*field) 
-                             . (shift gate)
+                            . (flip shift upper) 
+                            .  F.fft 
+                            . (*field) 
+                            . (shift gate)
     sz = LA.dim field
     upper = (divideInt sz 2)
     lower = (-1) * (divideInt sz 2) + 1
 
 
-mkPGgate :: ComplexSignal -> ComplexSignal
-mkPGgate = LA.mapVector ((**2) . abs)
+mkSigGate :: Nonlinearity -> ComplexSignal -> ComplexSignal -> (ComplexSignal, ComplexSignal)
+mkSigGate PG f g = (id f, LA.mapVector ((^2) . abs) g)
+mkSigGate SHG f g = (id f, id g) 
+mkSigGate SD f g = (LA.mapVector (^2) f, LA.mapVector LA.conjugate g)
+mkSigGate THG f g = (LA.mapVector (^2) f, id g)
 
-mkSHGgate :: ComplexSignal -> ComplexSignal
-mkSHGgate = id
-
-mkSDgate :: ComplexSignal -> ComplexSignal
-mkSDgate = LA.mapVector LA.conjugate
-
-mkTHGgate :: ComplexSignal -> ComplexSignal
-mkTHGgate = id
-
-
-mkPGfield :: ComplexSignal -> ComplexSignal
-mkPGfield = id
-
-mkSHGfield :: ComplexSignal -> ComplexSignal
-mkSHGfield = id
-
-mkSDfield :: ComplexSignal -> ComplexSignal
-mkSDfield = LA.mapVector (**2)
-
-mkTHGfield :: ComplexSignal -> ComplexSignal
-mkTHGfield = LA.mapVector (**2)
 
 
 shift :: ComplexSignal -> Int -> ComplexSignal
 shift gate delay = LA.buildVector sz (builder delay)
     where
     sz = LA.dim gate
-    builder d k = if k > (sz-1-d) then (getElem (k+d-sz)) else (getElem (k+d))
+    builder d k = if any (<0) [(sz-1-d-k), (k+d)] then (getElem (k+d-(signum d)*sz)) else (getElem (k+d))
         where
         getElem = (LA.@>) gate
+
+
+mkInitialGuess :: Int -> IO (LA.Vector Double)
+mkInitialGuess sz = do
+    stdgen <- R.getStdGen
+    let randMags = R.randomRs (0, 1.0) stdgen
+    let randAngs = R.randomRs (-pi, pi) stdgen
+    return $ LA.buildVector (2*sz) (\k -> if k < sz then (randMags !! k) else (randAngs !! k))
+
 
 divideInt :: Int -> Int -> Int
 divideInt n d = truncate ((fromIntegral n) / (fromIntegral d))
